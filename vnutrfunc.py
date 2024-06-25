f21023f51e7d1f95892898a805d62ae1aec21e41
mrpivan <152063033+mrpivan@users.noreply.github.com>
2024-06-25 09:32:24 +0300

    vnutrfunc.py

---
+++ vnutrfunc.py
@@ -0,0 +1,17 @@
+print('hello world')
+
+#TODO
+print('hello', 'world')
+
+#TODO
+print('first row')
+print('second row')
+
+
+'''-------------------------------'''
+
+input('пожалуйста напиши что то')
+
+#TODO
+name = input('напиши свое имя')
+print(name)
